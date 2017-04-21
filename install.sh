d63885948d3720d7ffa3ffd374080119544ee4b6
Haijun (Navy) Su <navysu@gmail.com>
2017-04-21 14:03:03 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# backup files
+if [ -e ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi 
+if [ -e ~/.vim ]; then
+    mv ~/.vim ~/.vim.bak
+fi
+
+# cp all files to home directory
+cp .vimrc ~/
+cp -r .vim ~/ 
+
+# clone vbundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# installing plugins
+vim +PluginInstall +qall
